f084843df7ae3ef271c1d965616b29dffad2244a
Erika Vidal <diann852@gmail.com>
2017-05-29 19:06:33 -0500

    ultimo commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ function initMap() {
          // Browser doesn't support Geolocation
          handleLocationError(false, infoWindow, map.getCenter());
        }
-     }
+
 
      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
        infoWindow.setPosition(pos);
@@ -38,51 +38,4 @@ function initMap() {
      }
 
 
-   var inputPartida=document.getElementById("punto-partida");
-     var inputDestino=document.getElementById("punto-destino");
-
-   new google.maps.places.Autocomplete(inputPartida);
-     new google.maps.places.Autocomplete(inputDestino);
-
-   var directionsService= new google.maps.DirectionsService;
-   var directionsDisplay= new google.maps.DirectionsRenderer;
-   var tarifa=document.getElementById("tarifa");
-
-     var calculateAndDisplayRoute= function(directionsService,directionsDisplay){
-       directionsService.route({
-         origin:inputPartida.value,
-         destination:inputDestino.value,
-       travelMode: "DRIVING"
-       }, function(response,status){
-           if(status==="OK"){
-             var distancia= Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
-
-             tarifa.classList.remove("none");
-             var costo=distancia*1.75;
-
-             if (costo<4) {
-               tarifa.innerHTML="S/.4";
-             } else {
-               tarifa.innerHTML="S/." + parseInt(costo);
-             }
-
-             console.log(response.routes[0].legs[0].distance.text);
-
-             if (miUbicacion !== undefined) {
-               miUbicacion.setMap(null);
-             }
-             directionsDisplay.setDirections(response);
-           }else{
-               window.alert("No encontramos una ruta");
-             }
-       });
- }
-     directionsDisplay.setMap(map);
-     markador.setMap(map);
-
-     var trazarRuta = function(){
-       calculateAndDisplayRoute(directionsService,directionsDisplay);
-     };
-
-     document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
- }
+}
