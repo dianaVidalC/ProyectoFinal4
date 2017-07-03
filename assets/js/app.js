d90238989c4d989451f63bb930a55e6fe7b00bc1
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-07-02 23:16:40 -0500

    trazar ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,88 +1,67 @@
-function initMap() {
 
-       var map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -34.397, lng: 150.644},
-         zoom: 18
-       });
-       var infoWindow = new google.maps.InfoWindow({map: map});
-       var markadorLaboratoria = new google.maps.Marker({
-           position: map,
-           map: map
-       });
+    function initMap() {
 
-       // Try HTML5 geolocation.
-       if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function(position) {
-           var pos = {
-             lat: position.coords.latitude,
-             lng: position.coords.longitude
-           };
+    var latitud,longitud;
 
-           infoWindow.setPosition(pos);
-           infoWindow.setContent('Location found.');
-           map.setCenter(pos);
-         }, function() {
-           handleLocationError(true, infoWindow, map.getCenter());
-         });
-       } else {
-         // Browser doesn't support Geolocation
-         handleLocationError(false, infoWindow, map.getCenter());
-       }
-     }
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat:-34.397,lng:150.644},
+            zoom: 3
+        });
 
-     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-       infoWindow.setPosition(pos);
-       infoWindow.setContent(browserHasGeolocation ?
-                             'Error: The Geolocation service failed.' :
-                             'Error: Your browser doesn\'t support geolocation.');
-     }
+        function ubicacion(){
 
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(markador);
+            } else {
+                alert('No se puede obtener la ubicaciòn');
+            }
+        }
 
-   var inputPartida=document.getElementById("punto-partida");
-     var inputDestino=document.getElementById("punto-destino");
+        function markador(position){
 
-   new google.maps.places.Autocomplete(inputPartida);
-     new google.maps.places.Autocomplete(inputDestino);
+            latitud  = position.coords.latitude;
+            longitud = position.coords.longitude;
 
-   var directionsService= new google.maps.DirectionsService;
-   var directionsDisplay= new google.maps.DirectionsRenderer;
-   var tarifa=document.getElementById("tarifa");
+            var marker= new google.maps.Marker({
+                position:{lat:latitud,lng:longitud},
+                map:map
 
-     var calculateAndDisplayRoute= function(directionsService,directionsDisplay){
-       directionsService.route({
-         origin:inputPartida.value,
-         destination:inputDestino.value,
-       travelMode: "DRIVING"
-       }, function(response,status){
-           if(status==="OK"){
-             var distancia= Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+            })
 
-             tarifa.classList.remove("none");
-             var costo=distancia*1.75;
+            map.setZoom(18);
+            map.setCenter({lat:latitud,lng:longitud});
+        }
 
-             if (costo<4) {
-               tarifa.innerHTML="S/.4";
-             } else {
-               tarifa.innerHTML="S/." + parseInt(costo);
-             }
+        var inputPartida     = document.getElementById("punto-partida");
+        var inputDestino     = document.getElementById("punto-destino");
 
-             console.log(response.routes[0].legs[0].distance.text);
+        new google.maps.places.Autocomplete(inputPartida);
+        new google.maps.places.Autocomplete(inputDestino);
 
-             if (miUbicacion !== undefined) {
-               miUbicacion.setMap(null);
-             }
-             directionsDisplay.setDirections(response);
-           }else{
-               window.alert("No encontramos una ruta");
-             }
-       });
- }
-     directionsDisplay.setMap(map);
-     markador.setMap(map);
+        var directionsService= new google.maps.DirectionsService;
+        var directionsDisplay= new google.maps.DirectionsRenderer;
+        
+        var calcularRuta=function (directionsService,directionsDisplay) {
 
-     var trazarRuta = function(){
-       calculateAndDisplayRoute(directionsService,directionsDisplay);
-     };
+            directionsService.route({
 
-     document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
- }
+                origin      : inputPartida.value,
+                destination : inputDestino.value,
+                travelMode  : 'DRIVING'
+            },function(response,status){
+                if (status=='ok'){
+                    directionsDisplay.setDirections(response);
+                }else{
+                    alert("No encontramos la ruta");
+                }
+            })
+            directionsDisplay.setMap(map);
+        }
+
+        var trazarRuta= function () {
+            calcularRuta(directionsService,directionsDisplay);
+        }
+
+        window.addEventListener("load",ubicacion);
+        document.getElementById("ruta").addEventListener("click",trazarRuta);
+    }
